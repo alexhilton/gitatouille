a70d8e6c8908651aa629f51f7c2f01772ddd76d4
Alex Hilton <dragonhilton@gmail.com>
2013-10-23 23:38:54 +0800

    Use Sherlock Action bar.
    Regard the API level resources.

---
+++ src/com/hilton/gitatouille/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.hilton.gitatouille;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class HomeActivity extends SherlockActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+}
