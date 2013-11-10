db28d54580d1a2389dbc4ae7ff6d3a5f55befd0f
Alex Hilton <dragonhilton@gmail.com>
2013-11-10 12:38:59 +0800

    Decouple the first level views with fragments.
    Each first level view would be a fragment. When select from side menu,
    show the corresponding fragment.

--- src/com/hilton/gitatouille/HomeActivity.java
+++ src/com/hilton/gitatouille/HomeActivity.java
@@ -1,11 +1,8 @@
 package com.hilton.gitatouille;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -16,16 +13,12 @@ import com.devspark.sidenavigation.SideNavigationView;
 public class HomeActivity extends SherlockFragmentActivity {
 
     private SideNavigationView mNavigationView;
-    private TextView mStubContent;
     private ActionBar mActionBar;
-    
-    private ExpandableListView mProgitListView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        mStubContent = (TextView) findViewById(R.id.content);
         mNavigationView = (SideNavigationView) findViewById(R.id.side_navigation_view);
         mNavigationView.setMenuItems(R.menu.main);
         mNavigationView.setMenuClickCallback(new ISideNavigationCallback() {
@@ -33,29 +26,22 @@ public class HomeActivity extends SherlockFragmentActivity {
             public void onSideNavigationItemClick(int itemId) {
                 switch (itemId) {
                 case R.id.menu_git_manual:
-                    mStubContent.setText(R.string.menu_git_manual);
                     mActionBar.setTitle(R.string.menu_git_manual);
                     break;
                 case R.id.menu_git_tutorial:
-                    mStubContent.setText(R.string.menu_git_tutorial);
                     mActionBar.setTitle(R.string.menu_git_tutorial);
                     showProGitList();
-                    mStubContent.setVisibility(View.GONE);
                     break;
                 case R.id.menu_git_tricks:
-                    mStubContent.setText(R.string.menu_git_tricks);
                     mActionBar.setTitle(R.string.menu_git_tricks);
                     break;
                 case R.id.menu_git_articles:
-                    mStubContent.setText(R.string.menu_git_articles);
                     mActionBar.setTitle(R.string.menu_git_articles);
                     break;
                 case R.id.menu_favorites:
-                    mStubContent.setText(R.string.menu_favorites);
                     mActionBar.setTitle(R.string.menu_favorites);
                     break;
                 case R.id.menu_about_us:
-                    mStubContent.setText(R.string.menu_about_us);
                     mActionBar.setTitle(R.string.menu_about_us);
                     break;
                 case R.id.menu_exit:
@@ -69,8 +55,6 @@ public class HomeActivity extends SherlockFragmentActivity {
         
         mActionBar = getSupportActionBar();
         mActionBar.setDisplayHomeAsUpEnabled(true);
-        
-        mProgitListView = (ExpandableListView) findViewById(R.id.progit_list);
     }
 
     @Override
@@ -93,19 +77,13 @@ public class HomeActivity extends SherlockFragmentActivity {
     }
 
     private void showProGitList() {
-        mProgitListView.setVisibility(View.VISIBLE);
-        final ProgitListAdapter adapter = new ProgitListAdapter(getBaseContext());
-        mProgitListView.setAdapter(adapter);
-        mProgitListView.setOnChildClickListener(new OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v,
-                    int groupPosition, int childPosition, long id) {
-                final ProGitSection section = adapter.getChild(groupPosition, childPosition);
-                final Intent i = new Intent(ContentViewerActivity.ACTION_VIEW_CONTENT);
-                i.putExtra(ProGit.EXTRA_URL, section.getUrl());
-                startActivity(i);
-                return true;
-            }
-        });
+        Fragment progit = getSupportFragmentManager().findFragmentByTag(ProGitFragment.TAG);
+        if (progit == null) {
+            progit = new ProGitFragment();
+        }
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_stub, progit, ProGitFragment.TAG);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
     }
 }
---
+++ src/com/hilton/gitatouille/ProGitFragment.java
@@ -0,0 +1,37 @@
+package com.hilton.gitatouille;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class ProGitFragment extends SherlockFragment {
+    public static final int ID = 0x001;
+    public static final String TAG = "progit";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        final View root = inflater.inflate(R.layout.progit, null, false);
+        final ExpandableListView list = (ExpandableListView) root.findViewById(R.id.progit_list);
+        final ProgitListAdapter adapter = new ProgitListAdapter(getActivity());
+        list.setAdapter(adapter);
+        list.setOnChildClickListener(new OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                    int groupPosition, int childPosition, long id) {
+                final ProGitSection section = adapter.getChild(groupPosition, childPosition);
+                final Intent i = new Intent(ContentViewerActivity.ACTION_VIEW_CONTENT);
+                i.putExtra(ProGit.EXTRA_URL, section.getUrl());
+                startActivity(i);
+                return true;
+            }
+        });
+        return root;
+    }
+}
