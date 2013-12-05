3dd681dc86c551433d51833984b5cc87ded8fed2
Alex Hilton <dragonhilton@gmail.com>
2013-12-05 23:40:31 +0800

    Remove list in list fragment.
    ListView in side ListView is a really bad ideas.

--- src/com/hilton/gitatouille/HomeActivity.java
+++ src/com/hilton/gitatouille/HomeActivity.java
@@ -29,10 +29,6 @@ public class HomeActivity extends SherlockFragmentActivity {
                     mActionBar.setTitle(R.string.menu_git_tutorial);
                     showProGitList();
                     break;
-                case R.id.menu_list_in_list:
-                    mActionBar.setTitle(R.string.menu_git_tutorial);
-                    showListInList();
-                    break;
                 case R.id.menu_exit:
                     finish();
                     break;
@@ -74,17 +70,6 @@ public class HomeActivity extends SherlockFragmentActivity {
         }
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_stub, progit, ProGitFragment.TAG);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
-    }
-    
-    private void showListInList() {
-        Fragment listInList = getSupportFragmentManager().findFragmentByTag(ProGitListFragment.TAG);
-        if (listInList == null) {
-            listInList = new ProGitListFragment();
-        }
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_stub, listInList, ProGitListFragment.TAG);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         ft.commit();
     }
