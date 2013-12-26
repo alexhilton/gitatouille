48687f93d59f7ec38463456fa909b0193bf645d1
Zhihui Gong <zhihuig@opera.com>
2013-12-26 13:35:35 +0800

    refactor the code generator

--- gitscm/view-content.py
+++ gitscm/view-content.py
@@ -2,6 +2,52 @@
 
 import json
 import re
+import io
+
+def generate_java_code(chaps):
+    print '\tList<ProGitChapter> chapters = new ArrayList<ProGitChapter>();'
+    chap_clazz = 'ProGitChapter '
+    sec_clazz = 'ProGitSection '
+    for c in chaps:
+        print '\t%s chap = new ProGitChapter("%s");' % (chap_clazz, c['title'])
+        for s in c['section']:
+            print '\t%ssec = new ProGitSection("%s", "%s");' % (sec_clazz, s['title'], s['filename'])
+            print '\tchap.addSection(sec);'
+            sec_clazz = ''
+        print '\tchapters.add(chap);'
+        chap_clazz = ''
+
+def encode(text):
+    return text.encode('utf8')
+
+def generate_index(chaps):
+    target = '../assets/progit/index2.html'
+    index = io.open(target, 'w', encoding = 'utf8')
+    index.write(unicode('''<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>Table of Contents</title>
+    <link rel="stylesheet" href="../jquery.mobile-1.3.2.min.css" type="text/css" charset="utf-8" />
+    <script src="../jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="../jquery.mobile-1.3.2.min.js" type="text/javascript" charset="utf-8"></script>
+</head>\n'''))
+    index.write(unicode('<body>\n'))
+    index.write(unicode('<div data-role="collapsible-set">\n'))
+    for c in chaps:
+        index.write(unicode('    <div data-role="collapsible">\n'))
+        index.write(unicode('        <h3><a href="#">%s</a></h3>\n' % c['title']))
+        index.write(unicode('        <div aria-hidden="true">\n'))
+        index.write(unicode('           <ul data-role="listview">\n'))
+        for s in c['section']:
+            index.write(unicode('             <li><a href="%s">%s</a></li>\n' % (s['filename'], s['title'])))
+        index.write(unicode('           </ul>\n'))
+        index.write(unicode('        </div>\n'))
+        index.write(unicode('    </div>\n'))
+    index.write(unicode('</div>\n'))
+    index.write(unicode('</body>\n'))
+    index.write(unicode('</html>'))
+    index.close()
 
 raw_content = json.load(open('gitscm.json', 'r'))
 
@@ -23,45 +69,4 @@ for item in raw_content:
         sec['filename'] = filename
         chaps[-1]['section'].append(sec)
 
-# generate Java codes
-print '\tList<ProGitChapter> chapters = new ArrayList<ProGitChapter>();'
-chap_clazz = 'ProGitChapter '
-sec_clazz = 'ProGitSection '
-for c in chaps:
-    print '\t%s chap = new ProGitChapter("%s");' % (chap_clazz, c['title'])
-    for s in c['section']:
-        print '\t%ssec = new ProGitSection("%s", "%s");' % (sec_clazz, s['title'], s['filename'])
-        print '\tchap.addSection(sec);'
-        sec_clazz = ''
-    print '\tchapters.add(chap);'
-    chap_clazz = ''
-
-# generate index.html
-#index = open('index.html', 'w')
-print('''<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <title>Table of Contents</title>
-    <link rel="stylesheet" href="../jquery.mobile-1.3.2.min.css" type="text/css" charset="utf-8" />
-    <script src="../jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
-    <script src="../jquery.mobile-1.3.2.min.js" type="text/javascript" charset="utf-8"></script>
-  </head>''')
-print('\n')
-print('<body>\n')
-print('<div data-role="collapsible-set">\n')
-for c in chaps:
-    print('    <div data-role="collapsible">\n')
-    print('        <h3><a href="#">%s</a></h3>\n' % c['title'])
-    print('        <div aria-hidden="true">\n')
-    print('           <ul data-role="listview">\n')
-    for s in c['section']:
-        print('             <li><a href="%s">%s</a></li>\n' % (s['filename'], s['title']))
-    print('           </ul>\n')
-    print('        </div>\n')
-    print('    </div>\n')
-print('</div>')
-print('\n')
-print('</body>\n')
-print('</html>')
-#index.close()
+generate_index(chaps)
